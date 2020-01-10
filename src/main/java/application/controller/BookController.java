7d8e9efdbdeaa97286e6c394762069d553c04ca4
Ferdis17 <fmuriyesu@gmail.com>
2020-01-10 11:51:05 -0500

    first commit for bookAPI project

---
+++ src/main/java/application/controller/BookController.java
@@ -0,0 +1,57 @@
+package application.controller;
+
+import application.exception.BookNotFoundException;
+import application.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import application.repository.BookRepository;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api")
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping("/book")
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping("/book")
+    public Book createBook(@Valid @RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @GetMapping("/book/{id}")
+    public Book getBookById(@PathVariable(value = "id") Long bookId) {
+        return bookRepository.findById(bookId).orElseThrow(() -> new BookNotFoundException("Book", "id", bookId));
+    }
+
+    @PutMapping("/book/{id}")
+    public Book updateBook(@PathVariable(value = "id") Long bookId,
+                           @Valid @RequestBody Book bookDetails) {
+
+        bookRepository.findById(bookId).orElseThrow(() -> new BookNotFoundException("Book", "id", bookId));
+
+        bookDetails.setId(bookId);
+        Book updateBook = bookRepository.save(bookDetails);
+        return updateBook;
+    }
+
+    @DeleteMapping("/book/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable(value = "id") Long bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new BookNotFoundException("Book", "id", bookId));
+
+        bookRepository.delete(book);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
